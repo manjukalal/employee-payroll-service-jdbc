73f5a30ad8d388f8a10f69a6d5386694e1d6b8b4
manjukalal <manjukalal1496@gmail.com>
2022-07-23 20:00:13 +0530

    Added: updated terisa salary using statement

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -1,21 +1,21 @@
 package com.bridgelabz;
 
 import java.sql.*;
+
 public class DBDemo {
     public static void main(String[] args) throws SQLException {
         System.out.println("welcome to employee-payroll mysql");
-
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
-
         String userName = "root";
-
         String password = "Mnj@1996";
+
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT *FROM employee ");
+            statement.execute("UPDATE employee SET salary=4000000.00 WHERE name='Terisa'");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee ");
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
             }
@@ -26,6 +26,4 @@ public class DBDemo {
         }
     }
 }
-
-
 
