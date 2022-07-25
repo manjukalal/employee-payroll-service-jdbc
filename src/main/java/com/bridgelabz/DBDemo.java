22bfa2a5b79591da05086314601c3412ae9a7023
manjukalal <manjukalal1496@gmail.com>
2022-07-25 09:15:36 +0530

    Added: retriving the particular data through datewise from database

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -13,12 +13,17 @@ public class DBDemo {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
-            PreparedStatement statement = connection.prepareStatement("UPDATE employee SET salary=5000000.00 WHERE name='Terisa'");
-            statement.executeUpdate();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee ");
+           Statement statement = connection.createStatement();
+//            PreparedStatement statement = connection.prepareStatement("UPDATE employee SET salary=5000000.00 WHERE name='Terisa'");
+//            statement.executeUpdate();
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee ");
+            ResultSet resultSet = statement.executeQuery(" select * from employee where start BETWEEN CAST('2020-02-05' AS DATE) AND DATE(NOW()); ");
+
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
+//                System.out.println("sum of the all  salary " + resultSet.getDouble("max(salary)"));
             }
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
