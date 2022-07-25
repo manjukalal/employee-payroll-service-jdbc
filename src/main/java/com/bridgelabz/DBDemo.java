39f28f4957d901dff7c5e7a7f2dcee7e68e7797f
manjukalal <manjukalal1496@gmail.com>
2022-07-25 21:11:27 +0530

    Added: inserting new employee data to database

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -16,12 +16,13 @@ public class DBDemo {
            Statement statement = connection.createStatement();
 //            PreparedStatement statement = connection.prepareStatement("UPDATE employee SET salary=5000000.00 WHERE name='Terisa'");
 //            statement.executeUpdate();
-            ResultSet resultSet = statement.executeQuery("SELECT sum(salary) FROM employee ");
+            statement.execute("insert into employee values (4,'vani','F',2000000,'2022-07-25')");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee ");
 //            ResultSet resultSet = statement.executeQuery(" select * from employee where start BETWEEN CAST('2020-02-05' AS DATE) AND DATE(NOW()); ");
 
             while (resultSet.next()) {
-//                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
-                System.out.println("sum of the all  salary " + resultSet.getDouble("sum(salary)"));
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
+//                System.out.println("sum of the all  salary " + resultSet.getDouble("sum(salary)"));
             }
 
         } catch (Exception e) {
