5d8ad6c6ae07693742c5e280427333e61fd0f67f
manjukalal <manjukalal1496@gmail.com>
2022-07-25 09:20:06 +0530

    Added: calculating sum,avg,min,max values from database through jdbc

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -16,12 +16,12 @@ public class DBDemo {
            Statement statement = connection.createStatement();
 //            PreparedStatement statement = connection.prepareStatement("UPDATE employee SET salary=5000000.00 WHERE name='Terisa'");
 //            statement.executeUpdate();
-//            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee ");
-            ResultSet resultSet = statement.executeQuery(" select * from employee where start BETWEEN CAST('2020-02-05' AS DATE) AND DATE(NOW()); ");
+            ResultSet resultSet = statement.executeQuery("SELECT sum(salary) FROM employee ");
+//            ResultSet resultSet = statement.executeQuery(" select * from employee where start BETWEEN CAST('2020-02-05' AS DATE) AND DATE(NOW()); ");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
-//                System.out.println("sum of the all  salary " + resultSet.getDouble("max(salary)"));
+//                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
+                System.out.println("sum of the all  salary " + resultSet.getDouble("sum(salary)"));
             }
 
         } catch (Exception e) {
